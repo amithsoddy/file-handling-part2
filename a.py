a1cd8ff68fc706df0bce636ffbfe0602092cc657
amithsoddy <anithasoddy@gmail.com>
2024-10-27 18:27:12 +0530

    file handling part 2

---
+++ a.py
@@ -0,0 +1,3 @@
+file=open('a.txt','w')
+file.write("neymar is the best dribller of all time he is from brazil tean many more legends are there from brazil like pele also declared as national treasure of the country")
+file.close()
